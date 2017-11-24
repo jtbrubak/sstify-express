cf708140cd7363539411f38d460dffa6c00997f3
Jeff Brubaker <jtbrubak1@gmail.com>
2017-11-23 23:43:52 -0500

    Add PlaylistTrack schema

--- models/playlist_track.js
+++ models/playlist_track.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var playlistTrackSchema = new mongoose.Schema({
-  
+  playlistOrd: Number
 });
 
 module.exports = mongoose.model('PlaylistTrack', playlistTrackSchema );
